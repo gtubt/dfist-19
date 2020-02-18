54610405152896497b7b5fe914ccad1eb5739a5b
Halil Onur Çeçen <halilonur.cecen@sahibinden.com>
2020-02-19 00:09:07 +0300

    updated a dependency

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\PC\Documents\dfist-19"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/halil.cecen/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/halil.cecen/repos/dfist-19"
+export "FLUTTER_TARGET=/Users/halil.cecen/repos/dfist-19/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/halil.cecen/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.5"
 export "FLUTTER_BUILD_NUMBER=5"
+export "TRACK_WIDGET_CREATION=true"
